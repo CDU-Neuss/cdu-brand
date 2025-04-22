e654b496b0a2d209103256e62ccd1eb282c8884c
Jens Küsters <jens@kuesters.net>
2025-04-22 22:17:33 +0200

    ✨ Add font imports and update version to 1.5.2 for improved typography

---
+++ docs/src/styles/fonts.js
@@ -0,0 +1,8 @@
+import '@fontsource/ibm-plex-serif/latin-400.css';
+import '@fontsource/ibm-plex-serif/latin-400-italic.css';
+import '@fontsource/ibm-plex-serif/latin-700.css';
+import '@fontsource/ibm-plex-serif/latin-700-italic.css';
+import '@fontsource/inter/latin-300.css';
+import '@fontsource/inter/latin-400.css';
+import '@fontsource/inter/latin-500.css';
+import '@fontsource/inter/latin-800.css';
